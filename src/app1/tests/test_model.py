1e9b1d186d3024837681a3f8859f80475451f11e
Atsuo Ishimoto <atsuoishimoto@users.noreply.github.com>
2024-09-18 15:22:00 +0900

    heh

---
+++ src/app1/tests/test_model.py
@@ -0,0 +1,9 @@
+from app1.models import Person
+import pytest
+
+@pytest.mark.django_db
+def test1():
+    p = Person(first_name="a", last_name="b")
+    p.save()
+
+    assert Person.objects.get(first_name="a")
